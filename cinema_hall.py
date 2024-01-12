0abe5ff9f0b1737bd062f19d1fb9fdbb5377021d
MohammadAminProgrammer2022 <mohammadaminkaramali777@gmail.com>
2024-01-11 20:45:54 -0800

    On CinemaHall class we added a new method, seat_number_generator and also some class attributes.

--- cinema_hall.py
+++ cinema_hall.py
@@ -4,9 +4,39 @@
 # ....
 # ################################################################
 
+import random
 
 class CinemaHall:
-    def __init__(self, name, total_seats, movies_showing):
+    # class attributes
+    all_users = {}
+    all_employees = {}
+    reserved_seats = [1, 2, 3, 4, 5, 6]
+    availabel_movies = {} # save structure ==> {movie_id : movie_obj}
+    
+    def __init__(self, name, total_seats):
         self.name = name
         self.total_seats = total_seats
-        self.movies_showing = movies_showing
+        # self.movies_showing = movies_showing
+    
+    def __str__(self):
+        pass
+    
+    def seat_number_generator(self):
+        '''
+            Recursive function which recevies total number of seats
+            and returns an available seat number.
+        '''
+        rand_num = random.randint(1, self.total_seats)
+        # print(rand_num)
+        if rand_num not in CinemaHall.reserved_seats:
+            CinemaHall.reserved_seats.append(rand_num)
+            return
+        else:
+            self.seat_number_generator()
+    
+        
+
+if __name__ == "__main__":
+    c1 = CinemaHall('c1', 10)
+    c1.seat_number_generator()
+    print(c1.reserved_seats)
